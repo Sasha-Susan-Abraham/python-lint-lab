d2658b63e8f7f52a664c2302fa23b32345ae8271
Sasha-Susan-Abraham <sashasusanabrahambtech24@rvu.edu.in>
2026-04-13 18:37:14 +0530

    Fix pylint warnings

--- calculator.py
+++ calculator.py
@@ -1,5 +1,9 @@
-def add(a, b):
+"""Simple calculator module."""
+
+def add(a: int, b: int) -> int:
+    """Return sum of two numbers."""
     return a + b
 
-def subtract(a, b):
+def subtract(a: int, b: int) -> int:
+    """Return difference of two numbers."""
     return a - b
