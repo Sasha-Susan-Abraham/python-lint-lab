a601b3ccc078ed00beecff60a39808793ba59790
Sasha-Susan-Abraham <sashasusanabrahambtech24@rvu.edu.in>
2026-04-13 18:52:24 +0530

    Fix pylint errors completely

--- calculator.py
+++ calculator.py
@@ -4,6 +4,7 @@ def add(a: int, b: int) -> int:
     """Return sum of two numbers."""
     return a + b
 
+
 def subtract(a: int, b: int) -> int:
     """Return difference of two numbers."""
     return a - b
